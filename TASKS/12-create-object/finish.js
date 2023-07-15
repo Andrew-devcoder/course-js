a8a695cb4e466ce3c4caade385a3cf4a04c26822
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-07-15 20:54:55 +0300

    done 12

--- TASKS/12-create-object/finish.js
+++ TASKS/12-create-object/finish.js
@@ -8,3 +8,21 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const infoPerson = {
+    name: "Andrew",
+    surname: "anonymous",
+    favoriteNumber: 13,
+};
+
+// dot notation
+console.log(
+    `My name is ${infoPerson.name} ${infoPerson.surname} and my favorite number is ${infoPerson.favoriteNumber}.`
+);
+
+// Destructuring
+const { name: firstName, surname, favoriteNumber } = infoPerson;
+
+console.log(
+    `My name is ${firstName} ${surname} and my favorite number is ${favoriteNumber}.`
+);
