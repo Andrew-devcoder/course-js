2d0489e36df46a5feb250e7ede805f70e57e0ddb
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-07-15 21:55:41 +0300

    15 done

--- TASKS/15-replace-part-string/finish.js
+++ TASKS/15-replace-part-string/finish.js
@@ -6,3 +6,25 @@
  *
  * 3. Выведите значение второй переменной в консоль
  */
+
+let message = "Good Morning";
+
+console.log(message);
+
+// разделаяем строку на слова с помощью переданного пробела в метод split
+const messageWord = message.split(" ");
+
+messageWord[1] = "Evening";
+
+message = messageWord.join(" ");
+
+console.log(message);
+
+// разделаяем строку на буквы с помощью переданного без пробела кавычки в метод split
+const messageLetters = message.split("");
+
+messageLetters[2] = "0";
+
+message = messageLetters.join("");
+
+console.log(message);
