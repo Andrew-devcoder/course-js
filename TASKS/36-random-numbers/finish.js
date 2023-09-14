40bd35e3acd1309657c47342b6bf5c53fd91f461
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-09-14 18:03:23 +0300

    change 36

--- TASKS/36-random-numbers/finish.js
+++ TASKS/36-random-numbers/finish.js
@@ -14,11 +14,45 @@
  *  - для добавления случайного числа в массив с возвратом измененного массива
  */
 
-let MIN = 1000;
-let MAX = 9999;
+// let MIN = 1000;
+// let MAX = 9999;
 
 // MIN = 10;
 // MAX = 1000;
+
+// const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351];
+
+// const genRandomNumber = (min, max) => {
+//     console.log("generation random number");
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+// const verification = (el) => {
+//     do {
+//         el = genRandomNumber(MIN, MAX);
+//     } while (String(el).length !== 4);
+
+//     console.log(`verification completed: ${el}`);
+//     return el;
+// };
+
+// const lengthTrue = verification();
+
+// const checkItem = (item) => {
+//     if (!myNumbers.includes(lengthTrue)) {
+//         return myNumbers.push(item);
+//     } else {
+//         console.log(`array have this item ${item}`);
+//         verification();
+//     }
+// };
+
+// checkItem(lengthTrue);
+
+// console.log(myNumbers);
+
+let MIN = 1000;
+let MAX = 9999;
 
 const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351];
 
@@ -27,26 +61,17 @@ const genRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const verification = (el) => {
-    do {
-        el = genRandomNumber(MIN, MAX);
-    } while (String(el).length !== 4);
-
-    console.log(`verification completed: ${el}`);
-    return el;
-};
-
-const lengthTrue = verification();
-
-const checkItem = (item) => {
-    if (!myNumbers.includes(lengthTrue)) {
-        return myNumbers.push(item);
+const genNewArr = (arr, min, max) => {
+    const newArray = [...arr];
+    const newNumber = genRandomNumber(min, max);
+    if (!newArray.includes(newNumber)) {
+        newArray.push(newNumber);
+        console.log(`new number ${newNumber}`);
+        console.log(newArray);
     } else {
-        console.log(`array have this item ${item}`);
-        verification();
+        console.log(`array have this number ${newNumber} try again...`);
+        genNewArr();
     }
 };
 
-checkItem(lengthTrue);
-
-console.log(myNumbers);
+genNewArr(myNumbers, MIN, MAX);
