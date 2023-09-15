684aafdd1af8c95816e91a81629366f0b492d0b4
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-09-15 14:37:31 +0300

    update 38

--- TASKS/40-map-json-to-object/finish.js
+++ TASKS/40-map-json-to-object/finish.js
@@ -10,8 +10,8 @@
  */
 
 const postsJSON = [
-  '{"postId":1355,"commentsQuantity":5}',
-  '{"postId":5131,"commentsQuantity":13}',
-  '{"postId":6134,"commentsQuantity":2}',
-  '{"postId":2351,"commentsQuantity":8}',
-]
+    '{"postId":1355,"commentsQuantity":5}',
+    '{"postId":5131,"commentsQuantity":13}',
+    '{"postId":6134,"commentsQuantity":2}',
+    '{"postId":2351,"commentsQuantity":8}',
+];
