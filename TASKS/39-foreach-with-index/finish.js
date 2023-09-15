54aa0550518e829fb88a5168d2c37d1505643c3e
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-09-15 14:45:03 +0300

    update 39

--- TASKS/39-foreach-with-index/finish.js
+++ TASKS/39-foreach-with-index/finish.js
@@ -17,6 +17,11 @@ const myCities = ["London", "New York", "Singapore"];
 //     }
 // };
 
+// const cityInfo = (city, index) =>
+//   `${city} is at the index ${index} in the myCities array`
+
+// myCities.forEach((city, index) => console.log(cityInfo(city, index)))
+
 const cityInfo = (array) => {
     array.forEach((element, index) => {
         console.log(
