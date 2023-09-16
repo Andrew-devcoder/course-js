9f07e5f134a216007bbbbfeeafa3577e88883407
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-09-16 19:50:02 +0300

    done 42

--- TASKS/42-is-array-sorted-or-not/finish.js
+++ TASKS/42-is-array-sorted-or-not/finish.js
@@ -11,15 +11,39 @@
  * 5. Если массив не отсортирован - вернуть "Массив не отсортирован"
  */
 
-const a = [5, 'abc', 10, 1]
-const b = [4, 10, 14, 25, 25, 50]
-const c = [150, 132, 80, 40]
-const d = [15, 26, 10, 23, 85]
+const a = [5, "abc", 10, 1];
+const b = [4, 10, 14, 25, 25, 50];
+const c = [150, 132, 80, 40];
+const d = [15, 26, 10, 23, 85];
 
-console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
-console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
-console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
-console.log(arraySortInfo(d)) // Массив не отсортирован
+const arraySortInfo = (inputArray) => {
+    let message = "";
+
+    if (inputArray.some((element) => typeof element !== "number")) {
+        message = "array have element typeof string";
+    } else if (
+        inputArray.every(
+            (element, index) => index === 0 || element >= inputArray[index - 1]
+        )
+    ) {
+        message = "array filter from least to most";
+    } else if (
+        inputArray.every(
+            (element, index) => index === 0 || element <= inputArray[index - 1]
+        )
+    ) {
+        message = "array filter from most to least";
+    } else {
+        message = "array not have filter";
+    }
+
+    return message;
+};
+
+console.log(arraySortInfo(a)); // Некоторые элементы не являются числами
+console.log(arraySortInfo(b)); // Массив отсортирован по возрастанию
+console.log(arraySortInfo(c)); // Массив отсортирован по убыванию
+console.log(arraySortInfo(d)); // Массив не отсортирован
 
 /**
  * ПОДСКАЗКИ
