74a2ee93c82d2648fe3011dd3e2b8ec92b0e6a62
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-11-02 17:32:13 +0200

    done 47

--- TASKS/47-reduce-array-of-objects/finish.js
+++ TASKS/47-reduce-array-of-objects/finish.js
@@ -7,25 +7,31 @@
  */
 
 const inputPosts = [
-  {
-    title: 'Как быстро выучить JavaScript?',
-    postId: 3421,
-    comments: 25,
-  },
-  {
-    title: 'Где используется JavaScript?',
-    postId: 5216,
-    comments: 3,
-  },
-  {
-    title: 'Какая разница между React и Angular?',
-    postId: 8135,
-    comments: 12,
-  },
-]
+    {
+        title: "Как быстро выучить JavaScript?",
+        postId: 3421,
+        comments: 25,
+    },
+    {
+        title: "Где используется JavaScript?",
+        postId: 5216,
+        comments: 3,
+    },
+    {
+        title: "Какая разница между React и Angular?",
+        postId: 8135,
+        comments: 12,
+    },
+];
 
-console.log(popularPostsIds(inputPosts, 10)) // [3421, 8135]
+const popularPostsIds = (post, minimalComentsQty) => {
+    return post
+        .filter((el) => el.comments > minimalComentsQty)
+        .map((el) => el.postId);
+};
 
-console.log(popularPostsIds(inputPosts, 15)) // [3421]
+console.log(popularPostsIds(inputPosts, 10)); // [3421, 8135]
 
-console.log(popularPostsIds(inputPosts, 50)) // []
+console.log(popularPostsIds(inputPosts, 15)); // [3421]
+
+console.log(popularPostsIds(inputPosts, 50)); // []
