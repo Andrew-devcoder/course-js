3e3920cd785295aa38fd8e85ab2befc8f4675c5e
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-11-02 18:26:54 +0200

    done 48

--- TASKS/48-reduce-to-object/finish.js
+++ TASKS/48-reduce-to-object/finish.js
@@ -8,35 +8,51 @@
  */
 
 const inputProducts = [
-  {
-    title: 'Phone case',
-    price: 23,
-    quantity: 2,
-    category: 'Accessories',
-  },
-  {
-    title: 'Android phone',
-    price: 150,
-    quantity: 1,
-    category: 'Phones',
-  },
-  {
-    title: 'Headphones',
-    price: 78,
-    quantity: 1,
-    category: 'Accessories',
-  },
-  {
-    title: 'Sport Watch',
-    price: 55,
-    quantity: 2,
-    category: 'Watches',
-  },
-]
+    {
+        title: "Phone case",
+        price: 23,
+        quantity: 2,
+        category: "Accessories",
+    },
+    {
+        title: "Android phone",
+        price: 150,
+        quantity: 1,
+        category: "Phones",
+    },
+    {
+        title: "Headphones",
+        price: 78,
+        quantity: 1,
+        category: "Accessories",
+    },
+    {
+        title: "Sport Watch",
+        price: 55,
+        quantity: 2,
+        category: "Watches",
+    },
+];
 
-console.log(quantitiesByCategories(inputProducts))
+const quantitiesByCategories = (inputPosts) => {
+    const result = {};
+
+    const accessories = inputPosts.filter((el) => el.category == "Accessories");
+    const phone = inputPosts.filter((el) => el.category == "Phones");
+    const watch = inputPosts.filter((el) => el.category == "Watches");
+
+    result["Accessories"] = accessories.length;
+    result["Phones"] = phone.length;
+    result["Watches"] = watch.length;
+
+    return result;
+};
+
+console.log(quantitiesByCategories(inputProducts));
 /* {
   Accessories: 3,
   Phones: 1,
   Watches: 2
 } */
+
+// не розумію чому тут в коментарі таки значення ключів - не вірні...
